88c0514461ce061c85bad298d36f91b08f47b5af
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-09-13 17:42:15 -0500

    styles

--- src/pages/User/userForm/UserForm.jsx
+++ src/pages/User/userForm/UserForm.jsx
@@ -3,7 +3,24 @@ import "./userForm.scss"
 export const UserForm = () => {
   return (
     <>
-        <h1>hello world</h1>
+        <header>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQX2XwnyPtF7Hz-ih0upJkh68hDDA03NbK-Ag&s" alt="Brand pic" />
+        </header>
+        <main>
+            <h1>Escribe tu nombre</h1>
+            <section>
+                <form>
+                    <input type="text" name="" id="" />
+                    <div>
+                        <button>Cuenta individual</button>
+                        <button>Cuenta compartida</button>
+                    </div>
+                    
+                </form>
+            </section>
+        </main>
+        
+
     </>
   )
 }
