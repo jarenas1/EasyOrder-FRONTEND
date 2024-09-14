577fd26a7b365e9234319dfb62133f47d03a0612
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-09-14 18:48:55 -0500

    footerToComponent

--- src/pages/Admin/login/Login.jsx
+++ src/pages/Admin/login/Login.jsx
@@ -13,15 +13,7 @@ const Login = () => {
                     <LoginForm/>
                 </section>
             </main>
-            <footer>
-                <section className="info">
-                    <a href=""><i className="bi bi-whatsapp"></i> WHATSAPP</a>
-                    <a href=""><i className="bi bi-instagram"></i> INSTAGRAM</a>
-                </section>
-                <section className="easy">
-                    <p>Producido por</p><img src='https://lh3.googleusercontent.com/29Z6d-M5XgTZQUIySfCKS73UKosvNP13auEb6pJq9AN8bxfzPrvFoKH4qLtqgVLkqJLJgQ=s85' alt="" />
-                </section>
-            </footer>
+           
             
     
         </>
