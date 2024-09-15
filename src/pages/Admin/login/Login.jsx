f44684a05af6fc4a22a8443eafa341603e697600
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-09-15 18:52:01 -0500

    buttonClick

--- src/pages/Admin/login/Login.jsx
+++ src/pages/Admin/login/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import LoginForm from './LoginForm'
 import './login.scss'
+import { Foot } from '../../../components/Foot'
+import { LoginForm } from './LoginForm'
 const Login = () => {
     return (
         <>
@@ -8,11 +9,14 @@ const Login = () => {
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQX2XwnyPtF7Hz-ih0upJkh68hDDA03NbK-Ag&s" alt="Brand pic" />
             </header>
             <main>
-                <h1>Escribe tu nombre</h1>
+                <h1>Iniciar sesion</h1>
                 <section>
                     <LoginForm/>
                 </section>
             </main>
+            <footer>
+                <Foot/>
+            </footer>
            
             
     
