a657c8b0554481de316616431ca7026f0ed03178
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-09-16 12:01:20 -0500

    mesasTable

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import { UserForm } from './pages/User/userForm/UserForm'
+import { Dashboard } from './pages/Admin/dashboard/Dashboard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-        <UserForm/>
+        <Dashboard/>
     </>
   )
 }
---
+++ src/pages/Admin/dashboard/Dashboard.jsx
@@ -0,0 +1,64 @@
+import './dashboard.scss'
+
+export  const Dashboard = () => {
+  return (
+    <>
+     <header>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQX2XwnyPtF7Hz-ih0upJkh68hDDA03NbK-Ag&s" alt="" />
+        <h2>Nombre del admin</h2>
+        <button>Cerrar sesion</button>
+      </header>
+      <main>
+        <h1>Panel de control</h1>
+        <section>
+            <article>
+                <p id='mesas' className='button-left' href="">Mesas</p>
+                <p id='productos' className='button-center' href="">Productos</p>
+                <a id='meseros' className='button-right' href="">Meseros</a>
+            </article>
+        </section>
+        <section>
+            <table>
+                <thead>
+                    <tr>
+                    <th>ID</th>
+                    <th>Numero</th>
+                    <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td>1229232asds021a</td>
+                    <td>12</td>
+                    <td>
+                        <button class="edit-btn">Editar</button>
+                        <button class="delete-btn">Eliminar</button>
+                    </td>
+                    </tr>
+
+                    <tr>
+                    <td>1229232asds021a</td>
+                    <td>12</td>
+                    <td>
+                        <button class="edit-btn">Editar</button>
+                        <button class="delete-btn">Eliminar</button>
+                    </td>
+                    </tr>
+
+                    <tr>
+                    <td>1229232asds021a</td>
+                    <td>12</td>
+                    <td>
+                        <button class="edit-btn">Editar</button>
+                        <button class="delete-btn">Eliminar</button>
+                    </td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
+      </main>
+    </>
+  )
+}
+
+
