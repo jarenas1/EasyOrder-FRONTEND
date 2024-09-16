88a72f4e0e43058035003083c71482294ae6ffba
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-09-15 19:23:59 -0500

    buttonOrder

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import { UserForm } from './pages/User/userForm/UserForm'
+import {ActiveSessions} from './pages/Waiter/acitveSessions/ActiveSessions'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-        <UserForm/>
+        <ActiveSessions/>
     </>
   )
 }
---
+++ src/pages/Waiter/acitveSessions/ActiveSessions.jsx
@@ -0,0 +1,12 @@
+export const ActiveSessions = () => {
+  return (
+    <>
+      <header>
+        <img src="" alt="" />
+        <h2>Nombre del mesero</h2>
+      </header>
+    </>
+  )
+}
+
+
