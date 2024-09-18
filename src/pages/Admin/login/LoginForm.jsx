3ef95b1e2e47ae5738146266f80fd8737e44eea4
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-09-18 16:48:42 -0500

    create connectionAPI

--- src/pages/Admin/login/LoginForm.jsx
+++ src/pages/Admin/login/LoginForm.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react"
-import useFetch from "../../../api/apiFetch";
 
 export const LoginForm = () => {
 
     const [form1, setForm1] = useState("");
     const [form2, setForm2] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
 
     //SET THE VALUE OF FORM IN THE STATE
     const handleInputChange = (e) => {
@@ -21,30 +21,52 @@ export const LoginForm = () => {
     }
 //juan@gmail.com
     //Check if the inputs are valid PONER ACA VALIDACIONNNNNNNN !!!!!!!!
-    const onSubmit = (e) => {
-        e.preventDefault()
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        
         // VALIDATE FORM DATA
-        if(form1 === "" || form2 === ""){
+        if (form1 === "" || form2 === "") {
             alert("Debes llenar todos los campos");
             return;
-        }else{
-            const {data} = useFetch("https://easyorder-backend-3.onrender.com/api/v1/auth/login", {
-                method: "POST",
-                body: JSON.stringify({
-                    email: form1,
-                    password: form2
-                })
-            })
-            console.log(data);
-            
         }
-    }
+        else{
+            setIsLoading(true);
+            try {
+                const response = await fetch("https://easyorder-backend-3.onrender.com/api/v1/auth/login", {
+                    method: "POST",
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        username: form1,
+                        password: form2
+                    })
+                });
+                console.log(response);
+                
+                if (!response.ok) {
+                    const errorText = await response.text();
+                    throw new Error('');
+                }
+        
+                const data = await response.json();
+                console.log(data);
+                
+            } catch (error) {
+                console.log(error);
+                
+                alert(`"Error during fetch: ${error.status}`);
+            }
+            setIsLoading(false);
+        }
+    };
     return (
         <>
             <form>
-                <input type="email" name="name" id="name" required placeholder="Ingresa tu email" onChange={handleInputChange}/>
-                <input type="password" name="password" id="name" required placeholder="Ingresa tu contraseña" onChange={handleInputChange}/>
-    
+                {/* <h1>{isLoading && "cargando"}</h1> */}
+                <input type="text" name="name" id="name" required placeholder="Ingresa tu email" onChange={handleInputChange}/>
+                <input type="password" name="password" id="passsword" required placeholder="Ingresa tu contraseña" onChange={handleInputChange}/>
+
                 <div>
                     <button type="submit" onClick={onSubmit}>Continuar</button>
                 </div>
