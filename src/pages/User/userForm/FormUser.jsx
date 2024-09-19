850804008ab6911da911f8a05310d788b5036a3f
PJuanTuiran <juanfer20091403@gmail.com>
2024-09-19 17:52:31 -0500

    Add the session

--- src/pages/User/userForm/FormUser.jsx
+++ src/pages/User/userForm/FormUser.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react"
+import { useNavigate } from "react-router-dom";
 import Swal from 'sweetalert2';
 
 export const Form = () => {
 
+    const navigate = useNavigate()
     const [status, setStatus] = useState(false) //ESTADO DE LA SESION INICIADA
     const [input, setInput] = useState("")
 
     //SE VA LLENANDO EL INPUT
     const onInputChange = ({target}) => {
-        console.log(target.value);
         setInput(target.value)
     }
     
@@ -26,16 +27,42 @@ export const Form = () => {
               })
         }else{
             setStatus(!status)
-            console.log(status);
         }
+    }
+
+    async function onSubmit(e) {
+        e.preventDefault()
+        if (!input) {
+            alert("deberias rellenar los cmabios")
+            return
+        }
+        const response = await fetch("https://easyorder-backend-3.onrender.com/api/v1/sessions", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name : input,
+                table_id: "f743890b-b2e0-459c-8603-1ead5fc0ff4b"
+            })
+        })
+
+        if (!response.ok) {
+            alert("Hubo un error")
+            
+        }
+        console.log(await response.json())
+        navigate("/products")
+        
+        
     }
   return (
     <>
-        <form>
+        <form onSubmit={onSubmit}>
             <input type="text" name="name" id="name" required placeholder="Ingresa tu nombre" onChange={onInputChange}/>
             <div>
-                <button type="submit" onClick={(e)=>changeStatus(e)}>Cuenta individual</button>
-                <button type="submit" onClick={(e)=>changeStatus(e)}> Cuenta compartida</button>
+                <button type="submit" >Cuenta individual</button>
+                <button type="submit" onClick={changeStatus}> Cuenta compartida</button>
             </div>
         </form>
     </>
