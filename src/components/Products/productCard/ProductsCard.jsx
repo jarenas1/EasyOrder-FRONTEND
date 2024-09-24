51a06d644234119484415f2231a4802ae5d2c619
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-09-24 12:32:47 -0500

    Create addToCartAlert

--- src/components/Products/productCard/ProductsCard.jsx
+++ src/components/Products/productCard/ProductsCard.jsx
@@ -1,11 +1,12 @@
-import React, { useContext } from 'react'
-import "./productsCard.scss"
-import {CartContext} from '../../../context/CartContext'
+import React, { useContext } from 'react';
+import "./productsCard.scss";
+import { CartContext } from '../../../context/CartContext';
+import Swal from 'sweetalert2'; // Importa SweetAlert2
 
 function ProductsCard({ product }) {
-  const { addToCart } = useContext(CartContext)
+  const { addToCart } = useContext(CartContext);
   
-  const {name, price, url} = product
+  const { name, price, url } = product;
 
   const formattedPrice = new Intl.NumberFormat('es-CO', {
     style: 'currency',
@@ -14,22 +15,32 @@ function ProductsCard({ product }) {
 
   return (
     <article className='section-showProducts__article'>
-        <figure className='section-showProducts__figure'>
-            <img src={url} alt=" Product Image" />
-        </figure>
-        <div className='section-showProducts__body'>
-            <h3>{name.toUpperCase()}</h3>
-            <p>{formattedPrice}</p>
-            <a href="" onClick={(e) => {
-               e.preventDefault()
-                addToCart(product)
-            }}>
-                <i className="bi bi-cart-plus"></i>
-            </a>
-        </div>
+      <figure className='section-showProducts__figure'>
+        <img src={url} alt="Product Image" />
+      </figure>
+      <div className='section-showProducts__body'>
+        <h3>{name.toUpperCase()}</h3>
+        <p>{formattedPrice}</p>
+        <a href="" onClick={(e) => {
+          e.preventDefault();
+          addToCart(product);
 
+          // Añade una alerta usando SweetAlert2
+          Swal.fire({
+            title: 'Producto añadido',
+            text: `${name} ha sido añadido al carrito.`,
+            icon: 'success',
+            timer: 2000,
+            showConfirmButton: false,
+            toast: true,
+            position: 'top-end'
+          });
+        }}>
+          <i className="bi bi-cart-plus"></i>
+        </a>
+      </div>
     </article>
-  )
+  );
 }
 
-export default ProductsCard
+export default ProductsCard;
