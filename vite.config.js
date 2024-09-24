d425cc4bbc5e7c33b78f600c0e5d8d15fe087890
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-09-24 13:18:30 -0500

    b

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Asegúrate de que esto sea correcto para tu ruta
+  build: {
+    outDir: 'dist', // Asegúrate de que esto coincida con lo que estás usando
+  },
 })
+
